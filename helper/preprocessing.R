14c2ab383785a03c831c3901220eeb7b861bc6bb
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2020-02-12 14:01:59 +0100

    checking for labs with missing data, language info added

--- helper/preprocessing.R
+++ helper/preprocessing.R
@@ -2,8 +2,10 @@ library(tidyverse)
 
 # READ_CDI
 # Function that detects file type and calls corresponding readr function
-read_cdi <- function(filename){
+read_cdi <- function(filename, save_unprocessed = F, verbose = F){
+  if(verbose){print(filename)}
   filetype <- str_split(filename, "\\.")[[1]][2]
+  # Define col_types for reading, except for standardized-score-CDI (to handle errors)
   col_types <- cols_only(
     labid = col_character(),
     subid = col_character(),
@@ -15,11 +17,53 @@ read_cdi <- function(filename){
     `CDI-error` = col_character(),
     Notes = col_character()
   )
+  # Read file
   df <- switch(filetype,
-               csv = read_csv(paste0("data/cdi/", filename), col_types = col_types),
-               tsv = read_tsv(paste0("data/cdi/", filename), col_types = col_types))
-  if(is_character(df$`standardized-score-CDI`)){
-    df$`standardized-score-CDI` = NA
+               csv = read_csv(paste0("data/cdi_raw/", filename), col_types = col_types),
+               tsv = read_tsv(paste0("data/cdi_raw/", filename), col_types = col_types)) %>%
+    as_tibble(.name_repair = "universal") # Replaces "-" by "." in column names
+  # Save unprocessed data into global file if needed (typically for the first run)
+  if(save_unprocessed){
+    unprocessed <- "data/unprocessed_cdi_merged.csv"
+    if(!file.exists(unprocessed)){
+      ## Initialise unprocessed merge file (types don't matter)
+      tibble(labid = character(),
+             subid = character(),
+             CDI.form = character(),
+             CDI.agerange = character(),
+             CDI.agedays = character(),
+             vocab_nwords = character(),
+             standardized.score.CDI = character(),
+             CDI.error = character(),
+             Notes = character(),
+             .rows = 0) %>%
+        write_csv(unprocessed)
+    }
+    ## Append data
+    write_csv(df, unprocessed, append = T)
   }
+  # Check standardized-score-CDI for input error
+  if(!is_double(df$standardized.score.CDI)){
+    df$standardized.score.CDI <- NA
+  }
+  return(df)
+}
+
+# READ_LABS
+# Read lab information from the sign-up sheet
+read_labs <- function(filename){
+  # Define col_types for columns to import
+  col_types <- cols_only(
+    `lab ID code` = col_character(),
+    `Language Community` = col_character()
+  )
+  # Read data, rename variables, recode language
+  df <- read_csv(filename, col_types = col_types) %>%
+    rename(labid = `lab ID code`,
+           language = `Language Community`) %>%
+    mutate(language_zone = case_when(str_detect(language, "British") ~ "British",
+                                     str_detect(language, "American|Canadian") ~ "NAE",
+                                     T ~ "Other")) %>%
+    mutate_all(as_factor)
   return(df)
 }
